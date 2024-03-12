0e9da8e08a04ec7b2f62367a17d197b424484ae7
Andy Flury <andrewflury@berkeley.edu>
2024-03-11 18:53:33 -0700

    Things are working now lol

--- public/workinNineToFive.js
+++ public/workinNineToFive.js
@@ -16,9 +16,20 @@ async function run() {
       // You must use the service worker notification to show the notification
       // Using new Notification("Hello World", { body: "My first notification on iOS"}) does not work on iOS
       // despite working on other platforms
+      console.log("Access granted");
+      $(document).on('click', '.showerBathToggle', (event) => {
+        toggleShowerBath(event);
 
-      $('.showerBathToggle').on('click', (e) => {
-        toggleShowerBath(e);
+      });
+
+      $(document).on('click', '.confirmBtn', (event) => {
+        handleConfirm(event);
+
+      });
+
+      $(document).on('click', '.addRowBtn', () => {
+        handleAddRow();
+
       });
       
 
@@ -34,12 +45,45 @@ async function run() {
 }
 
 function toggleShowerBath(event) {
-  const toggleContents = event.currentTarget.innerHTML;
+  // console.log(showerBathToggle);
+
+  // console.log(toggleContents);
+  const showerBathToggle = $(event.target);
+  const toggleContents = showerBathToggle.html();
   if (toggleContents === "bath") {
-    event.currentTarget.innerHTML = "shower";
+    showerBathToggle.html("shower");
   } else {
-    event.currentTarget.innerHTML = "bath";
+    showerBathToggle.html("bath");
   }
 }
+
+function handleConfirm(event) {
+  const confirmBtn = $(event.target);
+  const rowToConfirm = confirmBtn.parent().parent();
+  const guestNameCell = rowToConfirm.children('.guestNameCell');
+  const guestNameInput = guestNameCell.children('.guestNameInput');
+
+  if (confirmBtn.html() === "confirm") {
+    confirmBtn.html("edit");
+    guestNameCell.html(guestNameInput.val());
+  } else {
+    confirmBtn.html("confirm");
+    guestNameCell.html(`<input class="guestNameInput" value="${guestNameCell.html()}">`);
+  }
+}
+
+function handleAddRow() {
+  $('tbody').append(
+    `<tr>
+        <td class="guestNameCell"><input class="guestNameInput"></td>
+        <td><div class="showerBathToggle">bath</div></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td><div class="confirmBtn">confirm</div></td>
+    </tr>`
+  )
+}
+
 
 run();
